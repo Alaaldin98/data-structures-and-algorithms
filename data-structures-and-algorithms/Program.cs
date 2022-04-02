fd2a5ab6637a9f04f40d2bbe8a2fda74d71a1c14
alaaldin alhanini <98964675+Alaaldin98@users.noreply.github.com>
2022-04-02 17:05:00 +0200

    Correct solution & Add Readme

--- data-structures-and-algorithms/Program.cs
+++ data-structures-and-algorithms/Program.cs
@@ -2,10 +2,61 @@
 
 namespace data_structures_and_algorithms
 {
+    public class LinkedList
+    {
+        public Node head;
+        public void Insert(int value)
+        {
+            Node data = new Node(value);
+            data.next = head;
+            head = data;
+        }
+
+        public bool Include(int value)
+        {
+            Node current = head;
+            while (current != null)
+            {
+                if (value.Equals(current.value))
+                {
+                    return true;
+                }
+                current = current.next;
+            }
+            return false;
+        }
+
+        public string Tostring()
+        {
+            string stri = "";
+            Node current = head;
+            while (current != null)
+            {
+                stri += $"[ {current.value} ] -> ";
+                current = current.next;
+            }
+
+            stri += "NULL";
+            return stri;
+        }
+    }
+
+    public class Node
+    {
+        public int value;
+        public Node next;
+        public Node(int value)
+        {
+            this.value = value;
+            this.next = null;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            
+
 
             Console.WriteLine("Hello World!");
             /*     Console.WriteLine("input the length of your array");
