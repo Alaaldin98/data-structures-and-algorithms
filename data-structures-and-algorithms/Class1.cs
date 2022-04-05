7915c9f76c9d5cbc0b104151c07f792e422bb321
alaaldin alhanini <98964675+Alaaldin98@users.noreply.github.com>
2022-04-05 15:59:33 +0200

    solve conf

---
+++ data-structures-and-algorithms/Class1.cs
@@ -0,0 +1,64 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace data_structures_and_algorithms
+//{
+//    internal class Class1
+//    {
+//        public Node head { get; set; }
+//        public void Append(int value)
+//        {
+//            Node current = head;
+//            Node temp = new Node(value);
+
+//            while (current.next != null)
+//            {
+//                current = current.next;
+//            }
+//            current.next = temp;
+//        }
+//        public void InsertBefore(int beforValue, int newValue)
+//        {
+//            Node current = head;
+//            Node temp = new Node(newValue);
+//            while (current != null)
+//            {
+//                if (beforValue.Equals(current.value))
+//                {
+//                    temp.next = head;
+//                    head = temp;
+//                    break;
+//                }
+
+//                if (beforValue.Equals(current.next.value))
+//                {
+//                    temp.next = current.next;
+//                    current.next = temp;
+//                    break;
+//                }
+//                current = current.next;
+//            }
+
+//        }
+//        public void InsertAfter(int afterValue, int newValue)
+//        {
+//            Node current = head;
+//            Node temp = new Node(newValue);
+
+//            while (current != null)
+//            {
+//                if (afterValue.Equals(current.value))
+//                {
+//                    temp.next = current.next;
+//                    current.next = temp;
+//                    break;
+//                }
+//                current = current.next;
+//            }
+//        }
+//    }
+//}
+
