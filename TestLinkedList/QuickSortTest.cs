892f47977b3b0f08b7802fc4282bcc614f3f371e
alaaldin alhanini <98964675+Alaaldin98@users.noreply.github.com>
2022-06-14 07:23:36 +0200

    Solve hashmap repeated word

---
+++ TestLinkedList/QuickSortTest.cs
@@ -0,0 +1,64 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using Xunit;
+//using data_structures_and_algorithms.QuickSort;
+
+//namespace TestLinkedList
+//{
+//    public partial class QuickSortTests
+//    {
+
+//        [Fact]
+//        public void QuickSort_WithDistinctValues()
+//        {
+//            QuickSort()
+//        }
+
+
+//        [Fact]
+//        public void QuickSort_WithDuplicateValues()
+//        {
+//            var values = new List<int>(Constants.ArrayWithDuplicateValues);
+//            QuickSort.Sort(values, 0, values.Count - 1);
+//            Assert.IsTrue(UtilsTests.IsSortedAscendingly(values));
+//        }
+
+
+//        [Fact]
+//        public void QuickSort_WithSortedDistinctValues()
+//        {
+//            var values = new List<int>(Constants.ArrayWithSortedDistinctValues);
+//            QuickSort.Sort(values, 0, values.Count - 1);
+//            Assert.IsTrue(UtilsTests.IsSortedAscendingly(values));
+//        }
+
+
+//        [Fact]
+//        public void QuickSort_WithSortedDuplicateValues()
+//        {
+//            var values = new List<int>(Constants.ArrayWithSortedDuplicateValues);
+//            QuickSort.Sort(values, 0, values.Count - 1);
+//            Assert.IsTrue(UtilsTests.IsSortedAscendingly(values));
+//        }
+
+
+//        [Fact]
+//        public void QuickSort_WithReverselySortedDistinctValues()
+//        {
+//            var values = new List<int>(Constants.ArrayWithReverselySortedDistinctValues);
+//            QuickSort.Sort(values, 0, values.Count - 1);
+//            Assert.IsTrue(UtilsTests.IsSortedAscendingly(values));
+//        }
+
+
+//        [Fact]
+//        public void QuickSort_WithReverselySortedDuplicateValues()
+//        {
+//            var values = new List<int>(Constants.ArrayWithReverselySortedDuplicateValues);
+//            QuickSort.Sort(values, 0, values.Count - 1);
+//            Assert.IsTrue(UtilsTests.IsSortedAscendingly(values));
+//        }
+//    }
+//}
+
+
