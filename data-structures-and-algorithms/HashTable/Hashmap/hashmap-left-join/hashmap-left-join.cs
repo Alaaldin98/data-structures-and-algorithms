7cb86937b411a7f87fa02fd033f8116e30844c6f
alaaldin alhanini <98964675+Alaaldin98@users.noreply.github.com>
2022-06-25 22:10:44 +0200

    Solve left join

---
+++ data-structures-and-algorithms/HashTable/Hashmap/hashmap-left-join/hashmap-left-join.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_structures_and_algorithms.HashTable.Hashmap.hashmap_left_join
+{
+    public class hashmap_left_join
+    {
+        public static List<string> LeftJoin(HashTables left, HashTables right)
+        {
+            List<string> joinTable = new List<string>();
+
+            for (int i = 0; i < left.Table.Length; i++)
+            {
+                if (left.Table[i] != null)
+                {
+                    Node currentNode = left.Table[i];
+
+                    while (currentNode != null)
+                    {
+                        string concat = null;
+                        concat = concat + $"{currentNode.Key}, {currentNode.Value}, ";
+                        if (right.Contains(currentNode.Key))
+                        {
+                            concat = concat + right.Get(currentNode.Key);
+                        }
+                        else
+                        {
+                            concat += "NULL";
+                        }
+                        joinTable.Add(concat);
+                        currentNode = currentNode.Next;
+                    }
+                }
+            }
+
+            return joinTable;
+        }
+    }
+}
