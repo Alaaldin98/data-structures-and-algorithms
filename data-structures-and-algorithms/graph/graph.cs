88a036e9b10598a8c6cb1115cea561c07d975f55
alaaldin alhanini <98964675+Alaaldin98@users.noreply.github.com>
2022-06-26 00:12:37 +0200

    Add Graph

---
+++ data-structures-and-algorithms/graph/graph.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_structures_and_algorithms.graph
+{
+    public class graph
+    {
+        private List<Vertex> vertices;
+        private bool isWeighted;
+        private bool isDirected;
+        public graph(bool inputIsWeighted, bool inputIsDirected)
+        {
+            this.vertices = new List<Vertex>();
+            this.isWeighted = inputIsWeighted;
+            this.isDirected = inputIsDirected;
+        }
+        public Vertex AddNode(int val)
+        {
+            Vertex newvertex = new Vertex(val);
+            vertices.Add(newvertex);
+            return newvertex;
+        }
+        public void AddEdge(Vertex vertex1, Vertex vertex2, int weight)
+        {
+            if (!this.isWeighted)
+            {
+                weight = (int)(int?)null;
+            }
+            vertex1.addEdge(vertex2, weight);
+            if (!this.isDirected)
+            {
+                vertex2.addEdge(vertex1, weight);
+            }
+        }
+        public List<Vertex> GetNodes()
+        {
+            if (vertices.Count == 0)
+            {
+                return null;
+            }
+            else
+            {
+                return vertices;
+
+            }
+        }
+        public List<Edge> GetNeighbors(Vertex node)
+        {
+            return vertices.Find(v => v.value == node.value).edges;
+        }
+        public int Size()
+        {
+            return vertices.Count;
+        }
+
+        public void PrintGraph(List<int>[] adj, int V)
+        {
+            for (int v = 0; v < V; ++v)
+            {
+                Console.Write("vertex " + v + " ");
+                foreach (int x in adj[v])
+                    Console.Write("-> " + x);
+                Console.Write("\n");
+            }
+            Console.Write("\n");
+        }
+        public List<Vertex> BreadthFirst(Vertex vertex)
+        {
+            List<Vertex> visitedVertices = new List<Vertex>();
+            Queue<Vertex> visitQueue = new Queue<Vertex>();
+            List<Vertex> visited = new List<Vertex>();
+            visitQueue.Enqueue(vertex);
+            visited.Add(vertex);
+            if (visitQueue.Count == 0)
+            {
+                throw new Exception("no nodes added");
+            }
+            while (visitQueue.Count != 0)
+            {
+                Vertex front = visitQueue.Dequeue();
+                visitedVertices.Add(front);
+                foreach (Edge child in ((Vertex)front).edges)
+                {
+                    Vertex neighbor = child.getEnd();
+                    if (!visited.Contains(neighbor))
+                    {
+                        visited.Add(neighbor);
+                        visitQueue.Enqueue(neighbor);
+                    }
+                }
+            }
+            return visitedVertices;
+        }
+    }
+    public class Vertex
+    {
+        public int value { get; set; }
+        public List<Edge> edges;
+
+        public Vertex(int value)
+        {
+            this.value = value;
+            this.edges = new List<Edge>();
+        }
+        public void addEdge(Vertex endVertex, int weight)
+        {
+            this.edges.Add(new Edge(this, endVertex, weight));
+        }
+    }
+    public class Edge
+    {
+        private Vertex start;
+        private Vertex end;
+        private int weight;
+
+        public Edge(Vertex startV, Vertex endV, int inputWeight)
+        {
+            this.start = startV;
+            this.end = endV;
+            this.weight = inputWeight;
+        }
+
+        public Vertex getStart()
+        {
+            return this.start;
+        }
+
+        public Vertex getEnd()
+        {
+            return this.end;
+        }
+
+        public int getWeight()
+        {
+            return this.weight;
+        }
+    }
+}
---
+++ data-structures-and-algorithms/graph/graph.md
@@ -0,0 +1,43 @@
+﻿# Graphs
+A Graph is a non-linear data structure consisting of nodes and edges. The nodes are sometimes also referred to as vertices and the edges are lines or arcs that connect any two nodes in the graph.
+
+A Graph consists of a finite set of vertices(or nodes) and set of Edges which connect a pair of nodes.
+
+## Challenge
+ Graph implementation with folloing methods :
+
+- AddNode
+
+- AddEdge
+
+- GetNodes
+
+- GetNeighbors
+
+- Size
+
+## Approach & Efficiency
+|  Method   | 	Time Complexity  | 	Space Complexity  |
+|  -------  |  ----------        |  -------   |
+|  AddNode	| O(1)  |	O(1)  | 
+|  AddEdge	|  O(1)  | 	O(1)  |
+GetNodes  |	O(n)  | 	O(n)
+GetNeighbors  |	O(n)  |	O(1)
+Size  |	O(1)  |	O(1)
+
+## API
+
+1. AddNode()
+- Adds a node to the graph.
+
+2. AddEdge()
+- Adds an edge to a node which connects to another node or itself, and includes a weight value.
+
+3. GetNodes()
+- Returns a list of nodes within the graph.
+
+4. GetNeighbors()
+- Returns a list of neighbors of a given node, including the weight of each edge between them.
+
+5. Size()
+- Returns a count of all the nodes in a graph.
